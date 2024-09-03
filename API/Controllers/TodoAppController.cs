c158841903b530ed69ff80cceffea72babd0e41e
Ruben Madsen <42476677+rubenmadsen@users.noreply.github.com>
2024-09-03 13:58:32 +0200

    Moved database to DatabaseGenerator.cs

--- API/Controllers/TodoAppController.cs
+++ API/Controllers/TodoAppController.cs
@@ -12,28 +12,23 @@ using System;
 using System.IO;
 using System.Text.Json;
 using System.Text.Json.Serialization;
-
+using API.Manager;
 namespace API.Controllers
 {
-    public class Toxin
-    {
-        [JsonPropertyName("toxin")]
-        public string Name { get; set; }
-
-        [JsonPropertyName("description")]
-        public string Description { get; set; }
-    }
+    
     [Route("api/[controller]")]
     [ApiController]
     public class TodoAppController : ControllerBase
     {
         private readonly IConfiguration _configuration;
         private readonly ILogger<TodoAppController> _logger;
+        private DatabaseGenerator databaseManager;
 
         public TodoAppController(IConfiguration configuration, ILogger<TodoAppController> logger)
         {
             _configuration = configuration;
             _logger = logger;
+            this.databaseManager = new DatabaseGenerator(_configuration.GetConnectionString("DBcon"));
         }
 
         
@@ -127,236 +122,16 @@ namespace API.Controllers
         }
 
 
-        [ApiExplorerSettings(IgnoreApi = true)]
-        private float ConvertToGrams(string amount)
-        {
-            string[] parts = amount.Split(" ");
-            string unit = parts[1];
-            string num = parts[0];
-            if (num.StartsWith("<"))
-                num = num.Substring(1);
-            float.TryParse(num, out float value);
-
-                if (unit.Contains("mg"))
-                value /= 1000.0f; 
-            else if (unit.Contains("µg"))
-                value /= 1000000.0f; 
-            else if (unit.Contains("ng"))
-                value /= 1000000000.0f;
-
-            return value;
-        }
-
-
-
-        [ApiExplorerSettings(IgnoreApi = true)]
-        private void ClearTable(String tableName)
-        {
-            string sqlDatasource = _configuration.GetConnectionString("DBcon");
-            using (var connection = new SqliteConnection(sqlDatasource))
-            {
-                connection.Open();
-
-                string sql = $"DROP TABLE IF EXISTS {tableName};";
-
-                using (var command = new SqliteCommand(sql, connection))
-                {
-                    command.ExecuteNonQuery();
-                    Console.WriteLine($"Table {tableName} has been dropped.");
-                }
-                
-            }
-        }
-        [ApiExplorerSettings(IgnoreApi = true)]
-        private int AddToxin(String toxinName, String description, int category)
-        {
-
-            string sqlDatasource = _configuration.GetConnectionString("DBcon");
-            using (var connection = new SqliteConnection(sqlDatasource))
-            {
-                connection.Open();
-                string insertToxinQuery = "INSERT INTO Toxins (CategoryId, Name, Description) VALUES (@CategoryId, @Name, @Description)";
-                using (var insertToxinCommand = new SqliteCommand(insertToxinQuery, connection))
-                {
-                    insertToxinCommand.Parameters.AddWithValue("@CategoryId", category);
-                    insertToxinCommand.Parameters.AddWithValue("@Name", toxinName);
-                    insertToxinCommand.Parameters.AddWithValue("@Description", description);
-                    insertToxinCommand.ExecuteNonQuery();
-                    Console.WriteLine($"Toxin '{toxinName}' added under CategoryId {category}.");
-                }
-            }
-                return 0;
-        }
-        [ApiExplorerSettings(IgnoreApi = true)]
-        private int AddCategory(String categoryName)
-        {
-            int id = -1;
-            string sqlDatasource = _configuration.GetConnectionString("DBcon");
-            using (var connection = new SqliteConnection(sqlDatasource))
-            {
-                connection.Open();
-                string getCategoryQuery = "SELECT Id FROM Categories WHERE Name = @Name";
-                using (var getCategoryCommand = new SqliteCommand(getCategoryQuery, connection))
-                {
-                    getCategoryCommand.Parameters.AddWithValue("@Name", categoryName);
-                    var result = getCategoryCommand.ExecuteScalar();
-
-                    if (result == null)
-                    {
-                        // Step 2: If the category does not exist, insert it
-                        string insertCategoryQuery = "INSERT INTO Categories (Name) VALUES (@Name); SELECT last_insert_rowid();";
-                        using (var insertCategoryCommand = new SqliteCommand(insertCategoryQuery, connection))
-                        {
-                            insertCategoryCommand.Parameters.AddWithValue("@Name", categoryName);
-                            id = Convert.ToInt32(insertCategoryCommand.ExecuteScalar());
-                            Console.WriteLine($"Category '{categoryName}' inserted with Id {id}.");
-                        }
-                    }
-                    else
-                    {
-                        // The category exists, get the Id
-                        id = Convert.ToInt32(result);
-                        Console.WriteLine($"Category '{categoryName}' already exists with Id {id}.");
-                    }
-                }
-            }
-            return id;
-        }
-        [ApiExplorerSettings(IgnoreApi = true)]
-        private void ParseCSVToxins(String filePath)
-        {
-            String jsonPath = "../Data/toxin_descriptions.json";
-            String jsonString;
-            List<Toxin> toxins = new List<Toxin>();
-            using (StreamReader r = new StreamReader(jsonPath))
-            {
-                jsonString = r.ReadToEnd();
-                toxins = JsonSerializer.Deserialize<List<Toxin>>(jsonString);
-            }
-
-            using (var reader = new StreamReader(filePath))
-            {
-                string headerLine = reader.ReadLine();
-                string line;
-
-                int curCategoryId = -1;
-                string sqlDatasource = _configuration.GetConnectionString("DBcon");
-                using (var connection = new SqliteConnection(sqlDatasource))
-                {
-                    connection.Open();
-                    while ((line = reader.ReadLine()) != null)
-                    {
-                        var values = line.Split(',');
-                        string category = values[0];
-                        string toxin = values[1];
-
-                        if (!values[0].Equals(""))
-                            curCategoryId = this.AddCategory(category);
-
-                        if (toxin.Equals(""))
-                            toxin = category;
-                        Toxin tox = toxins.FirstOrDefault(t => t.Name.Equals(toxin));
-                        String desc = "None";
-                        if (tox != null)
-                            desc = tox.Description;
-                        else
-                            Debug.WriteLine($"Could not find a match for {toxin}");
-                        this.AddToxin(toxin, desc,  curCategoryId);
-                       
-                    }
-                }
-
-            }
-        }
-
-
-        [ApiExplorerSettings(IgnoreApi = true)]
-        private void ParseCSVConsumables(String filePath)
-        {
-            using (var reader = new StreamReader(filePath))
-            {
-                string headerLine = reader.ReadLine();
-                string line;
-
-                String lastCategory = "";
-                while ((line = reader.ReadLine()) != null)
-                {
-                    var values = line.Split(',');
-                    string substance;
-                    float cigPerPuff;
-                    float eCigPerPuff;
-                    Debug.WriteLine(line);
-                    if (!values[0].Equals(""))
-                        lastCategory = values[0];
-                    if (!values[1].Equals(""))
-                        substance = lastCategory;
-                    else
-                        substance = values[1];
-                    cigPerPuff = ConvertToGrams(values[2]);
-                    eCigPerPuff = ConvertToGrams(values[3]);
-
-                    Debug.WriteLine($"{lastCategory}, {substance}, {cigPerPuff}, {eCigPerPuff}");
-
-                }
-
-            }
-        }
-
-        [SwaggerOperation(Summary = "Clear the tables and regenerate and populate them.")]
+        [SwaggerOperation(Summary = "Generate the database from data files.")]
         [HttpGet]
         [Route("GenerateDatabase")]
         public IActionResult GenerateDatabase()
         {
-            string deleteAllToxinsQuery = "DELETE FROM Toxins";
-
-            string sqlDatasource = _configuration.GetConnectionString("DBcon");
-            using (var connection = new SqliteConnection(sqlDatasource))
-            {
-                connection.Open();
-                using (var deleteCommand = new SqliteCommand(deleteAllToxinsQuery, connection))
-                {
-                    int rowsAffected = deleteCommand.ExecuteNonQuery();
-                    Console.WriteLine($"{rowsAffected} rows deleted from the Toxins table.");
-                }
-            }
-            this.ClearTable("Categories");
-            this.ClearTable("Toxins");
-            using (var connection = new SqliteConnection(sqlDatasource))
-            {
-                connection.Open();
-
-                // Create table "Categories"
-                string createCategoriesTableQuery = @"
-                    CREATE TABLE IF NOT EXISTS Categories (
-                        Id INTEGER PRIMARY KEY AUTOINCREMENT,
-                        Name TEXT NOT NULL
-                    );";
-
-                using (var command = new SqliteCommand(createCategoriesTableQuery, connection))
-                {
-                    command.ExecuteNonQuery();
-                    Console.WriteLine("Table 'Categories' created successfully.");
-                }
-
-                // Create table Substances
-                string createToxinsTableQuery = @"
-                    CREATE TABLE IF NOT EXISTS Toxins (
-                        Id INTEGER PRIMARY KEY AUTOINCREMENT,
-                        CategoryId INTEGER NOT NULL,
-                        Name TEXT NOT NULL,
-                        Description TEXT,
-                        FOREIGN KEY (CategoryId) REFERENCES Categories(Id)
-                    );";
-                using (var command = new SqliteCommand(createToxinsTableQuery, connection))
-                {
-                    command.ExecuteNonQuery();
-                    Console.WriteLine("Table 'Categories' created successfully.");
-                }
-            }
-
-            this.ParseCSVToxins("../Data/toxins.csv");
-            //this.ParseCSVConsumables("../Data/toxins.csv");
-            return StatusCode(200, "All good in the hood.");
+            int result = this.databaseManager.GenerateDatabase();
+            if (result == 0)
+                return StatusCode(200, "All good in the hood.");
+            else
+                return StatusCode(500, "Grande problemas");
         }
 
     }

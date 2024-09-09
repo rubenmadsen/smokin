7995728fd9fa5a1c77ede620c3ebb82b558ce660
Ruben Madsen <42476677+rubenmadsen@users.noreply.github.com>
2024-09-09 13:50:57 +0200

    Added the three views

---
+++ Frontend/src/views/TrackerView.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+      <h1>Main view</h1>
+      <p>This is the tracker view.</p>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'TrackerView'
+  }
+  </script>
+  
+  <style scoped>
+  
+  </style>
+  

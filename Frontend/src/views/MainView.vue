3fde1ba17b55bd85d55869ec6f0aeb394752009a
coffeeBeansz <tomas.pousette@gmail.com>
2024-09-11 09:46:49 +0200

    fixat cig/ecig knapp och dessutom att skicka data till nästa sida

--- Frontend/src/views/MainView.vue
+++ Frontend/src/views/MainView.vue
@@ -1,27 +1,43 @@
 <template>
     <div class="mainContainer">
-        <div id="child1">
-            <router-link to="/info">
-                <h1>Det är svindåligt att röka</h1>            
-            </router-link>
-        </div>
-        <div id="child2">
-
-        </div>
-    </div>
-    <div id="goBtn">
-        <router-link to="/tracker">
-            <button>Link</button>
+      <div id="child1">
+        <input v-model="inputData" placeholder="Enter something" />
+        <router-link :to="{ path: '/info', query: { data: inputData } }">
+          <h1>Det är svindåligt att röka</h1>
         </router-link>
+        <p v-if="selectedButton">{{ selectedButton === 'first' ? 'You have selected Cigarette' : 'You have selected E-Cigarette' }}</p>
+      </div>
+      <div id="child2">
+        <button class="button-custom-1"
+                :class="{ 'button-pressed-1': selectedButton === 'first' }"
+                @click="toggleButton('first')">Cigarette</button>
+        <button class="button-custom-2"
+                :class="{ 'button-pressed-2': selectedButton === 'second' }"
+                @click="toggleButton('second')">E-Cigarette</button>
+      </div>
+      <div id="goBtn">
+        <router-link :to="{ path: '/tracker', query: { data: inputData } }">
+          <button>Link</button>
+        </router-link>
+      </div>
     </div>
-
   </template>
   
   <script>
-
   export default {
-    name: 'MainView'
-  }
+    name: 'MainView',
+    data() {
+      return {
+        inputData: '',  // This is your original input data
+        selectedButton: 'first' // First button by default, then it changes to the selected by user
+        };
+    },
+    methods: {
+      toggleButton(button) {
+        this.selectedButton = button; // Set the button as pressed
+      }
+    }
+  };
   </script>
   
   <style scoped>
@@ -40,5 +56,39 @@
     #goBtn{
         display: inline;
     }
-  </style>
+
+.button-custom-1 {
+  background-color: #3498db; /* Constant color */
+  color: white;
+  padding: 10px 20px;
+  border: none;
+  border-radius: 5px;
+  font-size: 16px;
+  cursor: pointer;
+  transition: background-color 0.3s; /* Smooth transition for color change */
+  margin-right: 0px; /* Corrected spacing */
+  margin-top: 0px;
+}
+
+.button-pressed-1 {
+  background-color: #2980b9; /* Darker color when pressed */
+}
+
+.button-custom-2 {
+  background-color: #3498db; /* Constant color */
+  color: white;
+  padding: 10px 20px;
+  border: none;
+  border-radius: 5px;
+  font-size: 16px;
+  cursor: pointer;
+  transition: background-color 0.3s; /* Smooth transition for color change */
+  margin-right: 0px; /* Corrected spacing */
+  margin-top: 0px;
+}
+
+.button-pressed-2 {
+  background-color: #2980b9; /* Darker color when pressed */
+}
+</style>
   

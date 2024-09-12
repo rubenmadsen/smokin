7e7aea665a1edf152d1a10f010e04db33eb26f6b
coffeeBeansz <tomas.pousette@gmail.com>
2024-09-12 11:34:02 +0200

    Lade till slider på trackersida

--- Frontend/src/views/TrackerView.vue
+++ Frontend/src/views/TrackerView.vue
@@ -10,11 +10,30 @@
     <div>
         <h2>Graph</h2>
     </div>
+
+    <div class="sliderContainer">
+      <p> How many cigarettes/e-cigarettes did you smoke today? </p>
+      <input v-model="sliderValue" type="range" min="0" max="100" class="slider"/>
+      <p>: {{ sliderValue }} cigarettes/e-cigarettes </p>
+
+      <button @click="handleSubmit" class="submitButton">Submit</button>
+    </div>
   </template>
   
   <script>
   export default {
-    name: 'TrackerView'
+    name: 'TrackerView',
+    data() {
+      return {
+        sliderValue: 50, // Added default slider value
+      };
+    },
+    methods: {
+      handleSubmit() {
+        // Logic to handle form submission
+        alert(`Submitted value: ${this.sliderValue}`);
+      },
+    },
   }
   </script>
   
@@ -31,6 +50,33 @@
     }
     .sumContainer last-child{
         background-color: aqua;
+    }
+
+    .sliderContainer {
+      margin-top: 20px;
+      text-align: center;
+      display: flex;
+      align-items: center; 
+    }
+
+    .slider {
+      width: 80%;
+      margin-right: 20px;
+    }
+
+    .submitButton { 
+      padding: 10px 20px;
+      background-color: #3498db;
+      color: white;
+      border: none;
+      border-radius: 5px;
+      cursor: pointer;
+      font-size: 16px;
+      transition: background-color 0.3s;
+    }
+
+    .submitButton:hover {
+      background-color: #2980b9;
     }
   </style>
   
